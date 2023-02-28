3518e5339f2f5f5ec48e3e2f1e730dace5b6c9cb
german-webdev <125234299+german-webdev@users.noreply.github.com>
2023-02-28 17:17:08 +1000

    Add files via upload

--- dist/js/promo.js
+++ dist/js/promo.js
@@ -1,18 +1,34 @@
-let button = document.querySelector('.promo__button');
+
+let promoGrid = document.querySelector('.promo__grid');
 let promoList = document.querySelector('.promo__list');
+let promoItem = document.querySelectorAll('.promo__item');
+let promoLink = document.querySelectorAll('.promo__link');
+let button = document.querySelector('.promo__button');
 let buttonArrow = document.querySelector('.button__arrow');
 let buttonText = document.querySelector('.button__content');
 
 button.addEventListener('click', () => {
     if (buttonText.textContent === 'Показать все') {
-        buttonText.textContent = 'Скрыть';
-        buttonArrow.classList.add('button__arrow--rotate');
+        arrowOpenRotate();
         promoList.classList.remove('promo__list--hidden');
-        
     } else {
-        buttonText.textContent = 'Показать все';
-        promoList.classList.add('promo__list--hidden');
-        buttonArrow.classList.remove('button__arrow--rotate');
+        arrowCloseRotate();
+        promoList.classList.add('promo__list--hidden'); 
         promoList.scrollTo(0, 0);
     } 
 });
+
+function arrowCloseRotate() {
+    window.setTimeout(function () {
+        buttonText.textContent = 'Показать все';
+        buttonArrow.classList.remove('button__arrow--rotate');
+    }, 100)
+}
+
+function arrowOpenRotate() {
+    window.setTimeout(function () {
+        buttonText.textContent = 'Скрыть';
+        buttonArrow.classList.add('button__arrow--rotate');
+    }, 200)
+}
+
--- dist/js/swiper.js
+++ dist/js/swiper.js
@@ -1,24 +1,60 @@
- 
-  // init Swiper:
-new Swiper('.swiper', {
 
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
+let slider = document.querySelector('.swiper');
+
+function mobileSlider() {
+  if (window.innerWidth <= 320) {
+    promoGrid.classList.remove('promo__grid');
+    promoList.classList.remove('promo__list');
+    promoList.classList.remove('promo__list--hidden');
+    arrowOpenRotate();
+
+    promoGrid.classList.add('swiper');
+    promoList.classList.add('swiper-wrapper');
+    promoItem.forEach(e => e.classList.add('swiper-slide'));
+
+    mySwiper = new Swiper(slider, {
+      direction: 'horizontal',
+      slidesPerView: 1.31,
+      spaceBetween: 16,
+      watchOverflow: true,
+      autoHeight: false,
+
+      pagination: {
+       	el: '.swiper-pagination',
         clickable: true,
-        dynamicBullets: false
-    },
+      },
 
-    simulateTouch: true,
-    grabCursor: true,
-
-    mousewheel: {
+        mousewheel: {
         sensitivity: 1,
         eventsTarget: '.swiper'
-    },
+      },
+ 
+    });
 
-    slidesPerView: 1.31,
-    spaceBetween: 16,
-    watchOverflow: true,
+    slider.dataset.mobile = 'true';
+  }
 
-  });
+  if (window.innerWidth > 320) {
+    slider.dataset.mobile = 'false';
+
+    if (slider.classList.contains('swiper-initialized')) {
+      mySwiper.destroy();
+
+    }
+    
+    promoGrid.classList.add('promo__grid');
+    promoList.classList.add('promo__list');
+    promoList.classList.add('promo__list--hidden');
+    arrowCloseRotate();
+
+    promoGrid.classList.remove('swiper');
+    promoList.classList.remove('swiper-wrapper');
+    promoItem.forEach(e => e.classList.remove('swiper-slide'));
+  }
+}
+
+mobileSlider();
+
+window.addEventListener('resize', () => {
+  mobileSlider();
+});
