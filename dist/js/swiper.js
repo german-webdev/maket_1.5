99414eac662cd8bd81b88edfa3536f8b6a578e6a
german-webdev <125234299+german-webdev@users.noreply.github.com>
2023-03-01 07:46:55 +1000

    Add files via upload

--- dist/js/swiper.js
+++ dist/js/swiper.js
@@ -1,60 +1,35 @@
-
-let slider = document.querySelector('.swiper');
-
 function mobileSlider() {
-  if (window.innerWidth <= 320) {
-    promoGrid.classList.remove('promo__grid');
-    promoList.classList.remove('promo__list');
-    promoList.classList.remove('promo__list--hidden');
-    arrowOpenRotate();
+  let init = false;
 
-    promoGrid.classList.add('swiper');
-    promoList.classList.add('swiper-wrapper');
-    promoItem.forEach(e => e.classList.add('swiper-slide'));
+  return () => {
+    if (window.innerWidth <= 320) {
+        if (!init) {
+          init = true;
+          swiper = new Swiper(".swiper", {
+              direction: 'horizontal',
+              width: 320,
+              spaceBetween: 16,
+              slidesPerView: 1.31,
 
-    mySwiper = new Swiper(slider, {
-      direction: 'horizontal',
-      slidesPerView: 1.31,
-      spaceBetween: 16,
-      watchOverflow: true,
-      autoHeight: false,
+              pagination: {
+                  el: ".swiper-pagination",
+                  clickable: true,
+              },
 
-      pagination: {
-       	el: '.swiper-pagination',
-        clickable: true,
-      },
-
-        mousewheel: {
-        sensitivity: 1,
-        eventsTarget: '.swiper'
-      },
- 
-    });
-
-    slider.dataset.mobile = 'true';
-  }
-
-  if (window.innerWidth > 320) {
-    slider.dataset.mobile = 'false';
-
-    if (slider.classList.contains('swiper-initialized')) {
-      mySwiper.destroy();
-
+              mousewheel: {
+                sensitivity: 1,
+                eventsTarget: '.swiper'
+              },
+          });
+        }
+    } else if (init) {
+      swiper.destroy();
+      init = false;
     }
-    
-    promoGrid.classList.add('promo__grid');
-    promoList.classList.add('promo__list');
-    promoList.classList.add('promo__list--hidden');
-    arrowCloseRotate();
-
-    promoGrid.classList.remove('swiper');
-    promoList.classList.remove('swiper-wrapper');
-    promoItem.forEach(e => e.classList.remove('swiper-slide'));
   }
 }
 
 mobileSlider();
 
-window.addEventListener('resize', () => {
-  mobileSlider();
-});
+window.addEventListener('resize', mobileSlider());
+window.addEventListener('load', mobileSlider());
